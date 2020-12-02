ca451d16c3b0b24bea092df5238f6469cfe74564
Matilda Arvidsson <matilda.j.arvidsson@gmail.com>
2020-12-02 16:41:21 +0100

    Add some comments

---
+++ huxflux.js
@@ -0,0 +1,13 @@
+// Här är filen du ska ändra i! Huxflux-funktionen ska returnera
+// en lista med 7 unika slumpmässiga heltal mellan 1 och 35. När
+// du öppnat projektet i browsern, kan du trycka på "Prova funktionen"
+// för att köra den och "Kör tester på funktionen" för att se om alla
+// krav uppfylls.
+
+const huxflux = () => {
+  return // Returnera din lista med nummer här
+}
+
+export {
+  huxflux
+}
