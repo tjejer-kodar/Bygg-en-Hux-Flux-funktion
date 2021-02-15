d1e362825d78f8f118264842087aa606dae1e4c2
Elin Amrén <65346494+elinamren@users.noreply.github.com>
2021-02-15 15:01:30 +0100

    Added huxflux function
    functionality and CSS

--- huxflux.js
+++ huxflux.js
@@ -3,13 +3,20 @@
 // du öppnat projektet i browsern, kan du trycka på "Prova funktionen"
 // för att köra den och "Kör tester på funktionen" för att se om alla
 // krav uppfylls.
-
 const huxflux = () => {
+  const numberArray = [];
+  for (let i = 0; i < 7; i++) {
+    let number = Math.floor(Math.random() * 35) + 1;
+    if (numberArray.includes(number) === false) {
+      numberArray.push(number);
+    } else {
+      i = i - 1;
+    }
+  }
+
   // Denna funktion behöver fyllas på med logik så att den returnerar
   // en lista med 7 unika slumpmässiga heltal mellan 1 och 35!
-  return
-}
+  return numberArray;
+};
 
-export {
-  huxflux
-}
+export { huxflux };
